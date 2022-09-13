e72f8a17b49fab6b69af33328ff97f265b3201d5
Steven! Ragnarök <steven@nuclearsandwich.com>
2022-09-12 22:20:26 -0700

    Drop resources directory which no longer has contents.

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 from setuptools import setup
-from glob import glob
 
 package_name = 'process_sarif'
 
@@ -7,10 +6,7 @@ setup(
     name=package_name,
     version='0.0.1',
     packages=[package_name],
-    data_files=[
-        ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name + "/resources", glob('resources/*'))
-    ],
+    data_files=[('share/' + package_name, ['package.xml'])],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='Eli Benevedes',
