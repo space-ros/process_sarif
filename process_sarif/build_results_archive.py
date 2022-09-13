b8fdedc7fd7dde17ff9f6fd05a5c28fff07e9c3f
Steven! Ragnarök <steven@nuclearsandwich.com>
2022-09-12 18:38:00 -0700

    Add console_script for generating a basic build_results_archive.
    This is the first full spike of a build_results_archive generator.
    Its current limitations are:
    
    * It must implicitly be run from the workspace root.
    * It takes no arguments to change the expected paths or names from their
      defaults.
    * It's not cleaning up the temporary files that it creates to archive.
    * It isn't timestamping the build_results_archive or creatng symlinks to
      a latest_build_results_archive.tar
    * No compression support yet although bzip2 should be Fine:tm:.
    * Internal API is not well factored.

---
+++ process_sarif/build_results_archive.py
@@ -0,0 +1,66 @@
+# Copyright 2022 Open Source Robotics Foundation
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+import pathlib
+import tarfile
+
+from .sarif import SarifFile
+from .sarif_helpers import remove_duplicate_results
+from typing import List
+
+
+def main():
+    # TODO(Steven!) make this path independent.
+    if not os.path.isdir('log/build_results_archives'):
+        os.makedirs('log/build_results_archives')
+    with tarfile.open('log/build_results_archives/current.tar', 'w') as archive:
+        sarif_files = [
+                SarifFile.from_path(sarif_path, verbose=True)
+                for sarif_path in
+                sorted(pathlib.Path('build').glob('**/*.sarif'))]
+
+        remove_duplicate_results(sarif_files)
+
+        with open('build-results-archive', 'w') as config:
+            config.write(
+                    '''
+                    { "version": 1 }
+                    ''')
+
+        archive.add('build-results-archive')
+
+        # TODO(Steven!) Add colcon-build-cmd, colcon-test-cmd, and process-sarif-cmd.
+        # TODO(Steven!) Add vcs-export-exact.repos
+
+        for sarif in sarif_files:
+            archive.add(sarif.path, recursive=True)
+            processed = processed_path(str(sarif.path))
+            processed_dir = os.path.dirname(processed)
+            if not os.path.isdir(processed_dir):
+                os.makedirs(processed_dir)
+            sarif.write_json(processed)
+            archive.add(processed, recursive=True)
+
+
+
+
+def processed_path(sarif_path: str):
+    return sarif_path.replace('build/', 'processed/')
+
+
+#def make_build_results_archive(sarif_files: List[SarifFile], processors):
+#    '''
+#    Create an archive of build results from the latest colcon bu
+#    '''
